138d374a63f380e6ac9132c58d410b4e6f669417
Eresia <bastien.lepesant@gmail.com>
2016-03-22 18:03:47 +0100

    Commentaire sur ReadCmd

--- ReadCmd.c
+++ ReadCmd.c
@@ -6,15 +6,25 @@ char** readCmd(){
 	char** result = calloc(NB_ARG_MAX+1, sizeof(char*));
 	int i;
 
+	//On lit la console
 	read(ENTER, cmd, SIZE_READ_MAX);
 
+	//On enlève le retour chariot
 	cmd[strlen(cmd)-1] = '\0';
+
+	//On lit le nom de la commande
 	param = strtok(cmd, " ");
 	i = 0;
-	
+
+	//Tant qu'il reste des paramètres et qu'on ne dépasse pas le nombre d'arguments
 	while((param != NULL) && (i < NB_ARG_MAX)){
+
+		//On ajoute le paramètre
 		result[i] = param;
+
+		//On en lit un nouveau
 		param = strtok(NULL, " ");
+
 		i++;
 	}
 
