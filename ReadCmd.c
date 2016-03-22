8b6bf637094f445c5a6aee3a26c17976daff576c
Eresia <bastien.lepesant@gmail.com>
2016-03-22 17:34:01 +0100

    Ajout d'une valeur nulle à la fin du tableau de paramètre

--- ReadCmd.c
+++ ReadCmd.c
@@ -3,19 +3,17 @@
 char** readCmd(){
 	char* cmd = calloc(SIZE_READ_MAX+1, sizeof(char));
 	char* param;
-	char** result = calloc(NB_ARG_MAX, sizeof(char*));
-	int enter = ENTER;
+	char** result = calloc(NB_ARG_MAX+1, sizeof(char*));
 	int i;
 
-	read(enter, cmd, SIZE_READ_MAX);
+	read(ENTER, cmd, SIZE_READ_MAX);
+
+	cmd[strlen(cmd)-1] = '\0';
 
 	param = strtok(cmd, " ");
 
 	i = 0;
 	while((param != NULL) && (i < NB_ARG_MAX)){
-		if(param[strlen(param)-1] == '\n'){
-			param[strlen(param)-1]  = '\0';
-		}
 		result[i] = param;
 		param = strtok(NULL, " ");
 		i++;
