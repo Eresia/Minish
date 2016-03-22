829c0aec335ee5c5716e36d9d26b97a43c34f63f
Bruno <bruno@cmitess.fr>
2016-03-22 18:03:17 +0100

    commentaire ESPACE git add --allgit add --allgit add --allgit add --allgit add --all

--- processExec.c
+++ processExec.c
@@ -7,11 +7,15 @@ int proccesExec(char** commande){
 		case -1 :
 			perror("Erreur : Création de processus");
 			return PROCESS;
-		case 0 : //Création du fils
+		case 0 :
+		 //Création du fils
+
 			printf("Execution de %s \n", cmd);
 			execvp(cmd, param); //Execution de la commande
 			printf("Le processus de PID %d à exécuté la commande %s\n", getpid(), cmd);
-			exit(NORMAL); // Fin du fils
+			exit(NORMAL);
+			
+			 // Fin du fils
 	}
 	return NORMAL;
 }
